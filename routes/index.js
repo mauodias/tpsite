9dc6865167e0522fe9162292d68392c0bc2226a0
Mauricio Dias <mau.dias@gmail.com>
2017-06-13 19:40:38 -0300

    Working

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,10 @@ router.get('/', function (req, res) {
     Order.findOne({
         closed: false
     }, function(err, order){
-        res.render('index', { user : req.user, order: order });
+
+        var suggested = setSuggested(order);
+        console.log(suggested);
+        res.render('index', { user : req.user, order: order, suggested: suggested});
     });
 });
 
@@ -48,7 +51,7 @@ router.post('/joinlist', function (req, res, next) {
             user: req.user,
             veggie: req.body.veggie ? true : false,
             eggs: req.body.eggs ? true : false,
-            beef: req.body.meat,
+            beef: req.body.meat == 'beef' ? true : false,
             drink: req.body.drink ? true : false
         }, function (err, entry){
             order.items.push(entry);
@@ -87,5 +90,64 @@ router.get('/removeuser', function (req, res, next) {
         res.redirect('/');
     });
 });
+
+function setSuggested(order){
+    console.log('setSuggested: '+ order)
+    var suggested = {
+        alabeef: 0,
+        alachicken: 0,
+        extrabeef: 0,
+        extrachicken: 0,
+        eggs: 0,
+        beans: 0,
+        drinks: 0
+    }
+
+    var total = order.items.length;
+    var countBeef = 0;
+    var countChicken = 0;
+    var countEggs = 0;
+    var countBeans = Math.ceil(total/15);
+    var countDrinks = 0;
+    var countAla = Math.round(total/4) > 0 ? Math.round(total/4) : 1
+
+    order.items.forEach(elem => {
+        if(!elem.veggie){
+            if(elem.beef){
+                countBeef++;
+            }
+            else {
+                countChicken++;
+            }
+        }
+        else {
+            if(elem.eggs){
+                countEggs++;
+            }
+        }
+        if(elem.drink){
+            countDrinks++;
+        }
+    });
+
+    if(countAla * 2 < total){
+        suggested.extrabeef = Math.ceil(countBeef/2);
+        suggested.extrachicken = Math.ceil(countChicken/2);
+    }
+
+    if(countAla <= countBeef){
+        countBeef -= countAla;
+        suggested.alabeef = countAla;
+    } else {
+        countChicken -= countAla;
+        suggested.alachicken = countAla;
+    }
+
+    suggested.eggs = countEggs + total - (2*countAla);
+    suggested.beans = countBeans;
+    suggested.drinks = Math.ceil(countDrinks / 5);
+
+    return suggested;
+}
 
 module.exports = router;
